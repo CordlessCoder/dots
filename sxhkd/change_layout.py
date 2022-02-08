b2a0b01523a3167bf187f2d99c9f3d98dd14bafd
CordlessCoder <42666308+CordlessCoder@users.noreply.github.com>
2022-02-08 17:31:15 +0200

    Delete change_layout.py

--- sxhkd/change_layout.py
+++
@@ -1,9 +0,0 @@
-import os
-
-layout = os.popen("setxkbmap -query", mode="r").read().split("\n")[2].split()[1]
-if layout == "us":
-    os.system("setxkbmap -layout ru")
-elif layout == "ru":
-    os.system("setxkbmap -layout ua")
-else:
-    os.system("setxkbmap -layout us")
