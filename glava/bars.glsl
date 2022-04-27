8c4558d75f246db44187f4a30f0d6e04231163cf
CordlessCoder <roman@vm.net.ua>
2022-04-27 15:25:07 +0300

    possibly starting a new rice

--- glava/bars.glsl
+++ glava/bars.glsl
@@ -11,7 +11,7 @@
 /* Outline width (in pixels, set to 0 to disable outline drawing) */
 #define BAR_OUTLINE_WIDTH 0
 /* Amplify magnitude of the results each bar displays */
-#define AMPLIFY 300
+#define AMPLIFY 150
 /* Whether the current settings use the alpha channel;
    enabling this is required for alpha to function
    correctly on X11 with `"native"` transparency. */
