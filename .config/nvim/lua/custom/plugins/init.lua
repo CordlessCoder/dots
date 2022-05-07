a360a6a885ce2abe06662cab85a3ef7426fa66dd
CordlessCoder <roman@vm.net.ua>
2022-05-07 18:46:48 +0300

    NvChad updates. These will be frequent.

---
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,65 @@
+-- custom/plugins/init.lua
+
+return {
+
+   ["elkowar/yuck.vim"] = {
+     ft = "yuck",
+   },
+   ["github/copilot.vim"] = {},
+   ["andweeb/presence.nvim"] = {
+     config = function()
+       require "presence":setup({
+         neovim_image_text = "I am still in your walls.",
+         buttons = ({{label = "I stole all of your code.", url = "https://github.com/CordlessCoder/dots"}, {label = "Pipe bomb is attached.", url = "https://github.com/CordlessCoder/Wallpapers"}})
+       })
+      end
+   },
+   ["Pocco81/TrueZen.nvim"] = {
+      cmd = {
+         "TZAtaraxis",
+         "TZMinimalist",
+         "TZFocus",
+      },
+   },
+     ["karb94/neoscroll.nvim"] = {
+       config = function()
+          require("neoscroll").setup({
+            easing_function = "sine",
+          })
+       end,
+
+       -- lazy loading
+       setup = function()
+         require("core.utils").packer_lazy_load "neoscroll.nvim"
+       end,
+     },
+     ["justinmk/vim-sneak"] = {},
+     ["f-person/git-blame.nvim"] = {},
+     ["ellisonleao/glow.nvim"] = {
+       branch = 'main',
+       setup = function()
+         require("core.utils").packer_lazy_load "glow.nvim"
+       end
+   },
+   ["iamcco/markdown-preview.nvim"] = {
+     setup = function()
+       require("core.utils").packer_lazy_load "markdown-preview.nvim"
+     end
+   },
+   ["averms/black-nvim"] = {
+       setup = function()
+         require("core.utils").packer_lazy_load "black-nvim"
+       end,
+    },
+    ["luukvbaal/stabilize.nvim"] = {
+    config = function()
+            require("stabilize").setup()
+        end,
+    },
+    ["jose-elias-alvarez/null-ls.nvim"] = {
+    after = "nvim-lspconfig",
+    config = function()
+       require("custom.plugins.null-ls").setup()
+      end,
+    },
+}
---
+++ .config/nvim/lua/custom/plugins/lspconfig.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.setup_lsp = function(attach, capabilities)
+   local lspconfig = require "lspconfig"
+
+   -- lspservers with default config
+   local servers = { "jedi_language_server", "html", "cssls", "clangd" }
+
+   for _, lsp in ipairs(servers) do
+      lspconfig[lsp].setup {
+         on_attach = attach,
+         capabilities = capabilities,
+      }
+   end
+end
+
+return M
