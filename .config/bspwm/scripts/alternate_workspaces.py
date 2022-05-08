5d64e4a9ca23e2bedc182f2c29625154510b3dd8
CordlessCoder <roman@vm.net.ua>
2022-05-08 18:50:02 +0300

    added an alternative workspace spanning script

---
+++ .config/bspwm/scripts/alternate_workspaces.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+
+
+names = ["I", "II", "III", "IV", "V", "VI", "VII", "IIX", "IX", "X"]
+monitors = os.popen("bspc query -M --names", mode="r").read().split("\n")
+monitors.pop()
+
+desktops = {}
+for x in monitors:
+    desktops[x] = []
+
+for i, x in enumerate(names):
+    desktops[monitors[i%len(monitors)]].append(x)
+
+for key in desktops.keys():
+    os.system(f"bspc monitor {key} -d {' '.join(desktops[key])}")
