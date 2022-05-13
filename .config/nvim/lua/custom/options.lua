784127a94d9c11fd4ea5a0bd2ed991f7066cacca
CordlessCoder <roman@vm.net.ua>
2022-05-13 12:12:11 +0300

    added Firefox CSS, .xinitrc

--- .config/nvim/lua/custom/options.lua
+++ .config/nvim/lua/custom/options.lua
@@ -2,6 +2,8 @@ local opt = vim.opt
 local g = vim.g
 
 opt.tabstop = 4
+opt.expandtab = true
+opt.shiftwidth = 4
 opt.autochdir = true
 g.toggle_theme_icon = ""
 g.instant_username = "CordlessCoder"
