fbb4455efc64933b997243b9f48d7c7428a12639
CordlessCoder <roman@vm.net.ua>
2022-05-20 00:27:10 +0300

    forgor to mount NvChad

---
+++ .config/nvim/lua/custom/options.lua
@@ -0,0 +1,10 @@
+local opt = vim.opt
+local g = vim.g
+
+opt.tabstop = 4
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.autochdir = true
+g.toggle_theme_icon = ""
+g.instant_username = "CordlessCoder"
+g.python3_host_prog = "/usr/bin/python3"
---
+++ .config/nvim/lua/custom/plugins/configs/init.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+M.treesitter = {
+   ensure_installed = {
+      "lua",
+      "html",
+      "css",
+      "python",
+      "markdown",
+      "bash",
+   },
+}
+
+M.telescope = {
+   extensions = {
+      -- fd is needed
+      media_files = {
+         filetypes = { "png", "webp", "jpg", "jpeg" },
+      },
+   },
+}
+
+return M
