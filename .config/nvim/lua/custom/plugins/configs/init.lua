6464ca3bb4fb17621626d1c4598571124e53c7d8
CordlessCoder <roman@vm.net.ua>
2022-05-30 22:12:36 +0300

    I'm very lost

---
+++ .config/nvim/lua/custom/plugins/configs/init.lua
@@ -0,0 +1,24 @@
+local M = {}
+
+M.treesitter = {
+	ensure_installed = {
+		"lua",
+		"html",
+		"css",
+		"python",
+		"markdown",
+		"bash",
+		"rust",
+	},
+}
+
+M.telescope = {
+	extensions = {
+		-- fd is needed
+		media_files = {
+			filetypes = { "png", "webp", "jpg", "jpeg" },
+		},
+	},
+}
+
+return M
