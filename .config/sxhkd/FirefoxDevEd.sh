1b586b71110a177f1830e1d06b4adb57dcb33932
CordlessCoder <roman@vm.net.ua>
2022-05-30 22:12:00 +0300

    removed functionality

--- .config/sxhkd/FirefoxDevEd.sh
+++ .config/sxhkd/FirefoxDevEd.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-if [ $(xdotool search --class 'firefoxdeveloperedition' getwindowpid | uniq | xargs -I{} xdotool search --all --pid {} --role 'browser' | wc -l) -ge 1 ]
-then 
-	xdo show $(xdotool search --name "Firefox Developer Edition" || echo "none")
-	bspc node $(xdotool search --name "Firefox Developer Edition" || echo "none") -g hidden=off &
-	xdotool search --class 'firefoxdeveloperedition' getwindowpid | uniq | xargs -I{} xdotool search --all --pid {} --role 'browser' windowactivate
-else
-	firefox-developer-edition &>/dev/null &
-fi
+xdotool search --class 'firefoxdeveloperedition' windowactivate || firefox-developer-edition &>/dev/null &
--- .config/sxhkd/NeoVim.sh
+++ .config/sxhkd/NeoVim.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ $(xdotool search --onlyvisible --name ' \- NVIM' | wc -l) -ge 1 ]
-then
-	xdo show $(xdotool search --name " \- NVIM" || echo "none")
-	bspc node $(xdotool search --name " \- NVIM" || echo "none") -g hidden=off &
-	xdotool search --onlyvisible --name ' \- NVIM' windowactivate
-else
-	neovide
-fi
+xdotool search --onlyvisible --name ' \- NVIM' windowactivate || neovide
